3086b343cf4259e71a1c22a868eff0c3d5fe4908
Sam Mason <s224443645@deakin.edu.au>
2024-09-25 14:37:49 +1000

    Fixing file structure

--- SIT232_7.1p/SIT232_1.2p/Debugging/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿for (var x = 0; x < 500;)
-{
-    Console.WriteLine(x);
-    x += 5;
-}
--- SIT232_7.1p/SIT232_5.1p/ZooPark/ZooPark.cs
+++
@@ -1,9 +0,0 @@
-﻿class ZooPark
-{
-    static void Main(string[] args)
-    {
-        Animal williamWolf = new Animal("Willian the World", "Meat", "Dog village", 50.6, 9, "Grey");
-        Animal tonyTgier = new Animal("Tony the tiger", "Meat", "Cate Land", 110, 6, "Orange and black");
-        Animal edgarEagle = new Animal("Edgar the Eagle", "Fish", "Bird Mania", 20, 15, "Black");
-    }
-}
